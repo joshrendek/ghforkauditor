6f675b1d504cb4eac7d7cadb4cedf7eb1878b173
Josh Rendek <josh@bluescripts.net>
2015-12-12 12:09:19 -0500

    Fix formatting on README and add godeps.

--- main.go
+++ main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/fatih/color"
+	"github.com/apcera/termtables"
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
 	"os"
+	"strings"
 )
+
+type repoInfo struct {
+	Name      string
+	Forks     int
+	ForkUsers []string
+	URL       string
+}
 
 func main() {
 	var (
@@ -19,7 +27,7 @@ func main() {
 	flag.Parse()
 
 	if len(oauth) == 0 || len(org) == 0 {
-		fmt.Println(color.RedString("oauth and org need to be set."))
+		fmt.Println("oauth and org need to be set.")
 		os.Exit(1)
 	}
 
@@ -36,17 +44,31 @@ func main() {
 		panic(err)
 	}
 
-	c := color.New(color.FgRed)
+	repositories := []repoInfo{}
 
 	for _, r := range repos {
 		name := *r.Name
 		forkCount := *r.ForksCount
-		if forkCount > 0 {
-			c.Printf("%s [%d] {%s} \n", name, forkCount, *r.Owner.Login)
+		isPrivate := *r.Private
+		url := fmt.Sprintf("https://github.com/%s/%s", org, name)
+		if forkCount > 0 && isPrivate {
 			forks, _, _ := client.Repositories.ListForks(*r.Owner.Login, name, &github.RepositoryListForksOptions{Sort: "newest"})
+			users := []string{}
 			for _, f := range forks {
-				c.Printf("\t %s \n", *f.Owner.Login)
+				users = append(users, *f.Owner.Login)
+
+			}
+			tmp := repoInfo{Name: name, Forks: forkCount, ForkUsers: users, URL: url}
+			if len(users) > 0 {
+				repositories = append(repositories, tmp)
 			}
 		}
 	}
+
+	table := termtables.CreateTable()
+	table.AddHeaders("Repository", "URL", "Forks", "Users")
+	for _, r := range repositories {
+		table.AddRow(r.Name, r.URL, r.Forks, strings.Join(r.ForkUsers, ", "))
+	}
+	fmt.Println(table.Render())
 }
